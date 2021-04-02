3e48b29f41a8a63827f97cb656cf6fd4f2e9be5c
Sneha Belkhale <sbelkhale@yahoo.com>
2021-04-02 13:41:52 -0700

    use map in pool

--- src/shaders/CausticFrag.glsl
+++ src/shaders/CausticFrag.glsl
@@ -5,17 +5,24 @@ uniform vec3 color1;
 uniform vec3 color2;
 uniform float timeMsec;
 uniform float alphaVal;
-varying vec3 vLightAdded;
 
+uniform sampler2D map;
+varying vec2 vUv;
 
 void main() {
+  vec4 mapColor = vec4(color1, 1.0);
+
+  #ifdef USE_MAP
+ 	mapColor = texture2D( map, vUv );
+ 	mapColor = mapTexelToLinear( mapColor );
+  #endif
   float noise1 = 0.5 + cnoise(0.5*vWorldPos + timeMsec);
   float col1 = pow(0.5 + 0.5 * sin(noise1*1.9*vWorldPos.x),8.0 + sin(timeMsec));
   float col3 = pow(0.5 + 0.5 * cos(noise1*2.1*vWorldPos.z)*cos(noise1*5.0*vWorldPos.z),8.0+ cos(timeMsec));
 
   float col = min(min( col1, col3),noise1) + 0.1*max(col1,col3);
 
-  gl_FragColor = vec4( mix(color1, color2, col) + 0.03*vLightAdded, alphaVal );
+  gl_FragColor = vec4( mix(mapColor.xyz, 1.2*color2, col), alphaVal );
 
 }
 
--- src/shaders/CausticVert.glsl
+++ src/shaders/CausticVert.glsl
@@ -2,33 +2,17 @@
 varying vec2 vUv;
 uniform float timeMsec;
 varying vec3 vWorldPos;
-varying vec3 vLightAdded;
 uniform float alphaVal;
-
-#include <common>
-#include <lights_pars_begin>
 
 void main() {
   vec4 worldPosition = modelMatrix * vec4(position, 1.0);
-  if(alphaVal < 1.0) {
-    worldPosition.y += 0.005*sin(20.0*worldPosition.x + 100.0*timeMsec);
-  }
-
+  // if(alphaVal < 1.0) {
+  //   worldPosition.y += 0.005*sin(20.0*worldPosition.x + 100.0*timeMsec);
+  // }
+  vUv = uv;
   vWorldPos = worldPosition.xyz;
   vWorldPos.x += 20.0;
   vWorldPos.z += 20.0;
   vec4 mvPosition = viewMatrix * worldPosition;
   gl_Position = projectionMatrix * mvPosition;
-
-  vec3 lightAdded = vec3(1.0, 1.0, 1.0);
-
-  #if ( NUM_DIR_LIGHTS > 0 )
-		DirectionalLight directionalLight;
-		for ( int i = 0; i < NUM_DIR_LIGHTS; i ++ ) {
-			directionalLight = directionalLights[ i ];
-			float lightAmt = saturate(dot(directionalLight.direction, normal));
-			lightAdded += lightAmt*directionalLight.color;
-		}
-	#endif
-  vLightAdded = lightAdded;
 }
