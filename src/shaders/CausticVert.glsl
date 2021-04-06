d6a751d62fc94adca62dbf39e3b388a8c9917f80
Sneha Belkhale <sbelkhale@yahoo.com>
2021-04-06 12:02:17 -0700

    specs on water

--- src/shaders/CausticVert.glsl
+++ src/shaders/CausticVert.glsl
@@ -2,14 +2,35 @@
 varying vec2 vUv;
 uniform float timeMsec;
 varying vec3 vWorldPos;
+varying vec3 vViewDir;
+varying vec3 vNormal;
 uniform float alphaVal;
+mat4 rotationMatrix(vec3 axis, float angle) {
+    axis = normalize(axis);
+    float s = sin(angle);
+    float c = cos(angle);
+    float oc = 1.0 - c;
+    
+    return mat4(oc * axis.x * axis.x + c,           oc * axis.x * axis.y - axis.z * s,  oc * axis.z * axis.x + axis.y * s,  0.0,
+                oc * axis.x * axis.y + axis.z * s,  oc * axis.y * axis.y + c,           oc * axis.y * axis.z - axis.x * s,  0.0,
+                oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c,           0.0,
+                0.0,                                0.0,                                0.0,                                1.0);
+}
 
+vec3 rotate(vec3 v, vec3 axis, float angle) {
+	mat4 m = rotationMatrix(axis, angle);
+	return (m * vec4(v, 1.0)).xyz;
+}
 void main() {
   vec4 worldPosition = modelMatrix * vec4(position, 1.0);
   vUv = uv;
+
   vWorldPos = worldPosition.xyz;
   vWorldPos.x += 20.0;
   vWorldPos.z += 20.0;
+  vNormal = normal;
+
   vec4 mvPosition = viewMatrix * worldPosition;
+  vViewDir = normalize(cameraPosition.xyz - worldPosition.xyz);
   gl_Position = projectionMatrix * mvPosition;
 }
