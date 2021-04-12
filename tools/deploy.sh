1d1f0f6510141d9fd840ae7b21cdde6dc1a718b5
kif11 <kovalewskiy@gmail.com>
2021-04-12 09:25:37 -0700

    Added deploy script

---
+++ tools/deploy.sh
@@ -0,0 +1,8 @@
+export NODE_ENV=production
+yarn build
+touch dist/.nojekyll
+git add dist/
+git commit -m "New github pages deploy"
+git subtree split --prefix dist -b gh-pages
+git push -f origin gh-pages:gh-pages
+git branch -D gh-pages
